98553fbdb52502590fe08c45a7394a637a0db631
Luiz Felipe Braga <80984666+Obrag@users.noreply.github.com>
2023-04-27 22:13:43 -0300

    Add files via upload

---
+++ Padrões de projeto/Facade/antipattern/src/Stream.java
@@ -0,0 +1,9 @@
+public class Stream{
+    public void conectar(){
+        System.out.println("Conectado");
+    }
+
+    public void iniciarStream(){
+        conectar();
+    }
+}
---
+++ Padrões de projeto/Facade/pattern/src/Stream.java
@@ -0,0 +1,6 @@
+public class Stream{
+    public void iniciarStream(){
+        Conexao conexao = new Conexao();
+        conexao.conectar();
+    }
+}
